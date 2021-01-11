601f26e5408f2b6a40ed722582b3a05dbf4e2377
gu4rd <rhrudgh12@Naver.com>
2021-01-11 21:58:20 +0900

    add login form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,30 @@
 import React, {Component} from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Login from "./login/login";
+import SignUp from "./login/signup";
 import Header from './layout/Header';
 import Body from './layout/Body';
 
 class App extends Component{
   render(){
-    return(
-      <div>
-        <Header/>
-        <Body/>
-      </div>
+    return(<Router>
+      <div className="App">
+        <div>
+          <Header/>
+          <Body/>
+        </div>
+        <div className="outer">
+          <div className="inner">
+            <Switch>
+              <Route path="/sign-in" component={Login} />
+              <Route path="/sign-up" component={SignUp} />
+            </Switch>
+          </div>
+        </div>
+      </div></Router>
     );
   }
 }
