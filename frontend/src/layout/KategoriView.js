de174b08bae8d8930db5e27164531636fe21b0a4
정아윤 <ptotmt5421@nate.com>
2021-01-21 21:35:32 +0900

    ''

--- frontend/src/layout/KategoriView.js
+++ frontend/src/layout/KategoriView.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import { Paper, Grid, Box } from "@material-ui/core";
 import ListItemText from "@material-ui/core/ListItemText";
@@ -27,25 +27,14 @@ const Item_width = {
   width: "75%",
 };
 
-const [number, setnumber] = useState(0);
-
-const onlncrease = () => {
-  setnumber(preNum => preNum + 1)
-}
-
-const onDecrease = () => {
-  setnumber(preNum => preNum - 1)
-}
-
 
 const Body = () => (
   <Wrapper>
     <div style={Kate_width}>
       <Grid container spacing={1}>
         <Grid item xs={12}>
-          <div>{number}</div>
-          <ListItemText button primary="BEST 100" onClick={onlncrease}></ListItemText>
-          <ListItemText button primary="의류" onClick={onDecrease}></ListItemText>
+          <ListItemText button primary="BEST 100"></ListItemText>
+          <ListItemText button primary="의류"></ListItemText>
           <ListItemText button primary="도서"></ListItemText>
           <ListItemText button primary="전자제품"></ListItemText>
           <ListItemText button primary="TODAY HOT"></ListItemText>
