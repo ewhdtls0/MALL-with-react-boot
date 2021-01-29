683d0d86f526d8e86f04c8c09445707301d052cf
정아윤 <ptotmt5421@nate.com>
2021-01-29 18:53:27 +0900

    Cart_count_layout

--- frontend/src/layout/component/HeaderComponents/views/Cart.js
+++ frontend/src/layout/component/HeaderComponents/views/Cart.js
@@ -1,25 +1,49 @@
-import React, {Component} from 'react';
-import styled from 'styled-components';
-import CartViews from './views/components/CartViews';
+import React, { Component } from "react";
+import styled from "styled-components";
+
+import CartViews from "./components/CartViews";
+import { numberWithCommas } from "./components/CartUtil";
 
 const Wrapper = styled.div`
-    height: 1020px;
-    width: 100%;
-    top: 60px;
-    z-index: 5;
+  height: 1020px;
+  width: 100%;
+  top: 60px;
+  z-index: 5;
 
-    color: black;
-    
-    border-top: 3px solid black;
-    font-size: 25px;
-    min-width: 1300px;
+  color: black;
+
+  border-top: 3px solid black;
+  font-size: 25px;
+  min-width: 1300px;
 `;
 
-class Cart extends Component{
-  render(){
-    return(
+class Cart extends Component {
+  state = {
+    order: {},
+    orderTotal: 0,
+  };
+
+  updateOrder = ({ name, price }) => {
+    var order = this.state.order;
+    order[name] = price;
+
+    var orderTotal = 0;
+    for (var key in order) {
+      orderTotal += order[key];
+    }
+
+    this.setState({
+      order,
+      orderTotal,
+    })
+  }
+
+  render() {
+    return (
       <Wrapper>
-        <CartViews/>
+        <CartViews
+          orderTotal={numberWithCommas(this.state.orderTotal)}
+          updateOrder={this.updateOrder} />
       </Wrapper>
     );
   }
--- frontend/src/layout/component/HeaderComponents/views/components/CartViews.js
+++ frontend/src/layout/component/HeaderComponents/views/components/CartViews.js
@@ -1,119 +1,128 @@
-import React, { Component } from 'react';
-import { Button, Grid } from '@material-ui/core';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import { Button, Grid } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
-import testImg1 from '../../../../img/product1.jpg'
-import testImg2 from '../../../../img/product2.jpg'
-import testImg3 from '../../../../img/product3.jpg'
+import CartCounter from "./CartCounter";
+import "../../../../css/CartCounter.css";
 
-const Wrapper = styled.div`
-    height: 1020px;
-    width: 100%;
-    top: 60px;
-    z-index: 5;
+import logoImg from "../../../../img/logo.png";
 
-    color: black;
-    
-    border-top: 3px solid black;
-    font-size: 25px;
-    min-width: 1300px;
-`;
+import testImg1 from "../../../../img/product1.jpg";
+import testImg2 from "../../../../img/product2.jpg";
+import testImg3 from "../../../../img/product3.jpg";
 
-const Cart_Width100 = {
-  width: '100%',
-}
+// const Wrapper = styled.div`
+//     height: 1020px;
+//     width: 100%;
+//     top: 60px;
+//     z-index: 5;
 
-const vertical_M = {
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  height: '30px',
-  color: 'black',
-}
+//     color: black;
 
-const ImgScale = {
-  height: "200px",
-  width: "200px",
-}
-class CartPages extends Component{
-  _removeCartView = async function() {
-    if(window.confirm('해당 상품을 장바구니에서 삭제하시겠습니까?')){
+//     border-top: 3px solid black;
+//     font-size: 25px;
+//     min-width: 1300px;
+// `;
+
+// const Cart_Width100 = {
+//   width: '100%',
+// }
+
+// const vertical_M = {
+//   display: 'flex',
+//   alignItems: 'center',
+//   justifyContent: 'center',
+//   height: '30px',
+//   color: 'black',
+// }
+
+// const ImgScale = {
+//   height: "200px",
+//   width: "200px",
+// }
+
+class CartPages extends Component {
+  _removeCartView = async function () {
+    if (window.confirm("해당 상품을 장바구니에서 삭제하시겠습니까?")) {
     }
+  };
+
+  render() {
+    return (
+      <div className="wrap">
+        <div>
+          <img className="logoImg" src={logoImg}></img>
+          <div className="prd-title">
+            <span className="prd-img">이미지</span>
+            <span className="prd-name">제품명</span>
+            <span className="prd-price">가격</span>
+            <span className="prd-quantity">수량</span>
+            <span className="prd-price-total">총계</span>
+          </div>
+          <CartCounter
+            seq={1}
+            name="헤드셋"
+            price={3000}
+            url={testImg1}
+            max={10}
+            updateOrder={this.props.updateOrder}
+          />
+          <CartCounter
+            seq={2}
+            name="시계"
+            price={3000}
+            url={testImg2}
+            max={10}
+            updateOrder={this.props.updateOrder}
+          />
+          <CartCounter
+            seq={3}
+            name="가방"
+            price={3000}
+            url={testImg3}
+            max={10}
+            updateOrder={this.props.updateOrder}
+          />
+          <br></br>
+          <div className="total-all">총 계 : {this.props.orderTotal}</div>
+        </div>
+      </div>
+    );
   }
-    render(){
-      return(
-        <Wrapper>
-          <h3>장바구니</h3>
-          <div style={Cart_Width100}>
-            <Grid container spacing={1} align="center">
-                <Grid item xs={2}><div style={vertical_M}>상품정보</div></Grid>
-                <Grid item xs={2}><div style={vertical_M}>판매자</div></Grid>
-                <Grid item xs={2}><div style={vertical_M}>가격</div></Grid>
-                <Grid item xs={2}><div style={vertical_M}>수량</div></Grid>
-                <Grid item xs={2}><div style={vertical_M}>합 계</div></Grid>
-            </Grid>
-          </div>
-          <div style={Cart_Width100}>
-            <Grid container spacing={1} align="center">
-                <Grid item xs={2}><img src={testImg1} style={ImgScale}></img><div style={vertical_M}>헤드셋</div></Grid>
-                <Grid item xs={2}><div style={vertical_M}>니키타</div></Grid>
-                <Grid item xs={2}><div style={vertical_M}>10000</div></Grid>
-                <Grid item xs={2}><div style={vertical_M}>1개</div></Grid>
-                <Grid item xs={2}><div style={vertical_M}>10000</div></Grid>
-                <Grid item xs={2}><Button onClick={() => this._removeCartView()}>삭제</Button></Grid>
-            </Grid>
-          </div>
-          <div style={Cart_Width100}>
-            <Grid container spacing={1} align="center">
-                <Grid item xs={2}><img src={testImg2} style={ImgScale}></img><div style={vertical_M}>시계</div></Grid>
-                <Grid item xs={2}><div style={vertical_M}>니키타</div></Grid>
-                <Grid item xs={2}><div style={vertical_M}>10000</div></Grid>
-                <Grid item xs={2}><div style={vertical_M}>1개</div></Grid>
-                <Grid item xs={2}><div style={vertical_M}>10000</div></Grid>
-                <Grid item xs={2}><Button onClick={() => this._removeCartView()}>삭제</Button></Grid>
-            </Grid>
-          </div>
-        </Wrapper>
-      );
-    }
-  }
+}
 
 export default CartPages;
 
-// html_Version
-// <table className="CartList_Table" align="center">
-//             <div>  
-//               <tr>
-//                 <th width="350">상품정보</th>
-//                 <th width="200">판매자</th>
-//                 <th width="200">가격</th>
-//                 <th width="200">수량</th>
-//                 <th width="200">합 계</th>
-//               </tr>
-//               <tr className="Item1">
-//                 <td><a href = "/"><img src={testImg1} style={ImgScale}></img></a><td>헤드셋</td></td>
-//                 <td>니키타</td>
-//                 <td>110000</td>
-//                 <td>1 개</td>
-//                 <td>110000</td>
-//                 <Button onClick={() => this._removeCartView()}>삭제</Button>
-//               </tr>
-//               <tr className="Item2">
-//                 <td><a href = "/"><img src={testImg2} style={ImgScale}></img></a><td>헤드셋</td></td>
-//                 <td>니키타</td>
-//                 <td>110000</td>
-//                 <td>1 개</td>
-//                 <td>110000</td>
-//                 <Button onClick={() => this._removeCartView()}>삭제</Button>
-//               </tr>
-//               <tr className="Item3">
-//                 <td><a href = "/"><img src={testImg3} style={ImgScale}></img></a><td>헤드셋</td></td>
-//                 <td>니키타</td>
-//                 <td>110000</td>
-//                 <td>1 개</td>
-//                 <td>110000</td>
-//                 <Button onClick={() => this._removeCartView()}>삭제</Button>
-//               </tr>
-//           </div>
-//           </table>
+// <h3>장바구니</h3>
+// <div style={Cart_Width100}>
+//   <Grid container spacing={1} align="center">
+//     <Grid item xs={2}><div style={vertical_M}>상품정보</div></Grid>
+//     <Grid item xs={2}><div style={vertical_M}>판매자</div></Grid>
+//     <Grid item xs={2}><div style={vertical_M}>가격</div></Grid>
+//     <Grid item xs={2}><div style={vertical_M}>수량</div></Grid>
+//     <Grid item xs={2}><div style={vertical_M}>합 계</div></Grid>
+//   </Grid>
+// </div>
+// <div style={Cart_Width100}>
+//   <CartCounter>
+//     <Grid container spacing={1} align="center">
+//       <Grid item xs={2}><img src={testImg1} style={ImgScale}></img><div style={vertical_M}>헤드셋</div></Grid>
+//       <Grid container display="flex" justify="center" direction="column" item xs={2}><div style={vertical_M}>니키타</div></Grid>
+//       <Grid container display="flex" justify="center" direction="column" item xs={2}><div style={vertical_M}>10000</div></Grid>
+//       <Grid container display="flex" justify="center" direction="column" item xs={2}><div style={vertical_M}>1개</div></Grid>
+//       <Grid container display="flex" justify="center" direction="column" item xs={2}><div style={vertical_M}>10000</div></Grid>
+//       <Grid container display="flex" justify="center" direction="column" item xs={2}><Button onClick={() => this._removeCartView()}>삭제</Button></Grid>
+//     </Grid>
+//   </CartCounter>
+// </div>
+// <div style={Cart_Width100}>
+//   <Grid container spacing={1} align="center">
+//       <Grid item xs={2}><img src={testImg2} style={ImgScale}></img><div style={vertical_M}>헤드셋</div></Grid>
+//       <Grid container display="flex" justify="center" direction="column" item xs={2}><div style={vertical_M}>니키타</div></Grid>
+//       <Grid container display="flex" justify="center" direction="column" item xs={2}><div style={vertical_M}>10000</div></Grid>
+//       <Grid container display="flex" justify="center" direction="column" item xs={2}><div style={vertical_M}>1개</div></Grid>
+//       <Grid container display="flex" justify="center" direction="column" item xs={2}><div style={vertical_M}>10000</div></Grid>
+//       <Grid container display="flex" justify="center" direction="column" item xs={2}><Button onClick={() => this._removeCartView()}>삭제</Button></Grid>
+//   </Grid>
+// </div>
