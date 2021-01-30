c7e625bc5b90acb16bf3d27ff1e85e97983c2f3e
feel0321 <dishate@naver.com>
2021-01-30 18:27:16 +0900

    상세 front
    상세 front

--- frontend/src/layout/component/MainViewComponents/views/components/ItemPages.js
+++ frontend/src/layout/component/MainViewComponents/views/components/ItemPages.js
@@ -8,34 +8,31 @@ const Wrapper = styled.div`
     z-index: 5;
 
     color: black;
-    
     border-top: 3px solid black;
     font-size: 25px;
     min-width: 1300px;
-`;
-/*
-const ItemPages = ({match}) => {
-  const [users, setUsers] = useState([]);
-  useEffect(() => {
-      fetch("/boardOne?title=" + match.params.title)
-        .then((response) => response.json())
-        .then((users) => {
-          setUsers(users)
-        })
-  }, [])
 
+    padding: 20px;
+`;
+const Rule = ({ color }) => (
+  <hr
+    style={{
+      borderColor: color,
+    }}
+  />
+);
+
+const ItemPages = ({match}) => {
   return (
     <Wrapper>
-      {users.map((user) => (
-        <div>
-          <h5>{user.title}</h5>
-          <h5>{user.content}</h5>
-          <h5>{user.writer}</h5>
-        </div>
-      ))}
+        <h5>제목</h5>
+        <h5 className="float-right">작성자</h5>
+        <br/>
+        <Rule color="blue" />
+        <h5>내용</h5>
+        <img src="https://w.namu.la/s/52cdde81ca492970bebd8d422bd57f3a0733fec9a9051948d23776e01956265d7a1c10974012343675e4809474a9e2fd4ca6da10adb882eebecbd81f4576635a49e66b76d794a8fb882fadff3554698faa7f95ae6d49048fd775e9daaf61adee" width="400"></img>
     </Wrapper>
   )
 }
 
 export default ItemPages;
-*/
