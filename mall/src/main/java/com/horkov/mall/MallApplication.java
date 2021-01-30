7b63dd83f9c7dbdd265cf49ba974381fc888b7d8
gu4rd <rhrudgh12@Naver.com>
2021-01-30 22:25:09 +0900

    User CRUD

--- mall/src/main/java/com/horkov/mall/MallApplication.java
+++ mall/src/main/java/com/horkov/mall/MallApplication.java
@@ -1,10 +1,5 @@
 package com.horkov.mall;
 
-import javax.sql.DataSource;
-
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,17 +8,6 @@ public class MallApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MallApplication.class, args);
-	}
-	@Bean
-	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
-		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
-		sessionFactory.setDataSource(dataSource);
-
-		Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
-
-		sessionFactory.setMapperLocations(res);
-		
-		return sessionFactory.getObject();
 	}
 
 }
